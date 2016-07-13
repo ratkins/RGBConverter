3f00897e39153d95b1de9c8447b42495a4374a7f
randysuen <randy.suen@outlook.com>
2016-07-13 15:38:11 +0800

    fix bugs by using float instead of int

--- RGBConverter.cpp
+++ RGBConverter.cpp
@@ -68,9 +68,9 @@ void RGBConverter::hslToRgb(double h, double s, double l, byte rgb[]) {
     } else {
         double q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         double p = 2 * l - q;
-        r = RGBConverter::hue2rgb(p, q, h + 1/3);
+        r = RGBConverter::hue2rgb(p, q, h + 1/3.0);
         g = RGBConverter::hue2rgb(p, q, h);
-        b = RGBConverter::hue2rgb(p, q, h - 1/3);
+        b = RGBConverter::hue2rgb(p, q, h - 1/3.0);
     }
 
     rgb[0] = r * 255;
@@ -162,8 +162,8 @@ double RGBConverter::threeway_min(double a, double b, double c) {
 double RGBConverter::hue2rgb(double p, double q, double t) {
     if(t < 0) t += 1;
     if(t > 1) t -= 1;
-    if(t < 1/6) return p + (q - p) * 6 * t;
-    if(t < 1/2) return q;
-    if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    if(t < 1/6.0) return p + (q - p) * 6 * t;
+    if(t < 1/2.0) return q;
+    if(t < 2/3.0) return p + (q - p) * (2/3.0 - t) * 6;
     return p;
 }
